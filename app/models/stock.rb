6f188ab9d5bc21d9aec8c27fb19c4c9d751f436c
pmann08 <partulmann@gmail.com>
2022-07-09 18:48:35 +0530

    Addition of stocks to portfolio

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,8 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true
 
   #class method - no dependency on instance variable
   def self.new_lookup(ticker_symbol)
@@ -11,5 +15,9 @@ class Stock < ApplicationRecord
     rescue => exception
       return nil
     end
+  end
+
+  def self.check_db(ticker_symbol)
+    where(ticker: ticker_symbol).first
   end
 end
